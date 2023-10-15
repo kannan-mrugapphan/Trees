8abf23b33cd547d172b5a29d4b95a798cdb08460
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-10-15 00:37:09 -0500

    Update DiamaterOfTree.java

--- DiamaterOfTree.java
+++ DiamaterOfTree.java
@@ -1,35 +1,34 @@
 // 543.
-// case 1 - diamater passes through root => left subtree height + right subtree height + 1 (for the root) is the number of nodes in this path
-// case 2 - diamater doesnt pass through root
-// find diamater of left and right sub trees and return max 
-// base - diamater of empty tree is 0
-// time - O(n^2)
-// space - O(h)
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        //base
-        if(root == null)
-        {
-           return 0; 
-        }
-        //logic
-        int diameterRoot = findHeight(root.left) + findHeight(root.right);
-        int lDiameter = diameterOfBinaryTree(root.left);
-        int rDiameter = diameterOfBinaryTree(root.right);
-        return Math.max(diameterRoot, Math.max(lDiameter, rDiameter));
+        int[] result = new int[]{Integer.MIN_VALUE};
+        findHeight(root, result);
+        return result[0];
     }
-    
-    //find height method retuns height of tree
-    //base - height of empty tree is 0
-    //time - O(n)
+
+    //diameter may or may not pass through root
+    //length of longest path through a node = left subtree height + right subtree height
+    //find length of longest path through all nodes and max among them is the diameter
+    //i/p - root, result[] of size 0 where 0th index is diameter of tree
+    //time - O(n) height of tree code
     //space - O(h)
-    private int findHeight(TreeNode root) {
+    private int findHeight(TreeNode root, int[] result)
+    {
         //base
+        //if root is null at start or becomes null due to a parent call
         if(root == null)
         {
-            return 0;
+            return 0; //height of empty tree
         }
-        //logic
-        return 1 + Math.max(findHeight(root.left), findHeight(root.right));
+
+        int leftSubtreeHeight = findHeight(root.left, result);
+        int rightSubtreeHeight = findHeight(root.right, result);
+        //find length of longest path through root
+        int lps = leftSubtreeHeight + rightSubtreeHeight;
+        //update result
+        result[0] = Math.max(result[0], lps);
+
+        //return height of root
+        return 1 + Math.max(rightSubtreeHeight, leftSubtreeHeight);
     }
 }
