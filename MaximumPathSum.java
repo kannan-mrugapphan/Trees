6a2324795fa0c45b16ace5d010ef559d664d0b48
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-10-15 01:18:41 -0500

    Update MaximumPathSum.java

--- MaximumPathSum.java
+++ MaximumPathSum.java
@@ -1,45 +1,45 @@
 // 124.
-
-//post order traversal as a node is processed only after knowing values from its left and right sub trees
-//a node can belong to a path 
-//max path sum for such a path till that node = node value + max(value from left subtree, value from right sub tree)
-//this node can also be starting node of a path = node value
-//case1 = max(node value, node value + max(values from left and right subtrees))
-
-//a node can be a root of a path
-//case2 = node value + value from left sub tree + value from right sub tree
-
-//case 3 = a node is not part of max path 
-
-//have a global result and update it with max(case1, case2, current global result)
-
 class Solution {
-    int result = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        //edge
+        int[] result = new int[] { Integer.MIN_VALUE };
+        findMaxSum(root, result);
+        return result[0];
+    }
+
+    //i/p - root, result[] where 0th index is max path sum in tree
+    //time - O(n) - find height code
+    //space - O(h)
+    private int findMaxSum(TreeNode root, int[] result)
+    {
+        //base
+        //if root is null at start or becomes null due to call
         if(root == null)
         {
             return 0;
         }
-        findMaxPath(root);
-        return result;
-    }
-    
-    private int findMaxPath(TreeNode root) {
-        //base
-        if(root == null) //max path sum in an empty tree is 0
-        {
-            return 0;
-        }
-        //logic
-        int left = findMaxPath(root.left); //find max path sum from right and left sub trees
-        int right = findMaxPath(root.right);
-        
-        //handle 3 cases
-        int case1 = Math.max(root.val + Math.max(left, right), root.val);
-        int case2 = Math.max(case1, root.val + left + right);
-        result = Math.max(result, case2);
-        
-        return case1; //return case1 to handle that case when current root is not part of max path (case 3)
+
+        //max sum through current root is root's val + max sum from left + max sum from right
+        int leftSum = Math.max(0, findMaxSum(root.left, result));
+        int rightSum = Math.max(0, findMaxSum(root.right, result));
+
+        //    10
+        //   /  \
+        //  -3  -4
+        //leftSum = -3, rightSum = -4, maxSumThroughRoot = 10
+        //so make leftSum and rightSum as 0 (ignore them) when they are -ve
+        //theoretically the case maxSumThroughRoot ignoring both children
+
+        //compute max sum through root
+        int maxSumThroughRoot = leftSum + rightSum + root.val;
+        result[0] = Math.max(result[0], maxSumThroughRoot); //update result
+
+        //to caller, root can either attach with left subtree or right subtree because a path is needed, the other option is it can ignore both and return just itself
+        //  node -> root -> left
+        //               \
+        //                -> right
+
+        //possible paths are either node -> root -> left or node -> root -> right
+        //return path with max sum
+        return Math.max(root.val + leftSum, root.val + rightSum);
     }
 }
