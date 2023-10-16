67a847d57008fbdf734274f15936e71d299293ae
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-10-15 21:59:34 -0500

    Update serializeAndDeserializeTree.java

--- serializeAndDeserializeTree.java
+++ serializeAndDeserializeTree.java
@@ -1,58 +1,136 @@
-// 297.
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+//297.
 public class Codec {
-    
-    //time - O(n) with call stack space = O(h)
+
+    Queue<String> serializedTree = new LinkedList<>();
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        StringBuilder serializedString = new StringBuilder(); //tracks the str version of tree
+        serializeHelperIterative(root, serializedString);
+
+        serializedString.deleteCharAt(serializedString.length() - 1); //last delimiter is not needed
+
+        return serializedString.toString();
+    }
+
+    //time - O(n) - preorder traversal
+    //space - O(h)
+    private void serializeHelper(TreeNode root, StringBuilder serializedString)
+    {
         //base
         if(root == null)
         {
-            return "X,"; //, is a delimiter o distinguisg between "123" where boundary is confusing and "1,2,3" whwre 1 is root 2 is left child and 3 is right child
+            serializedString.append("#,"); //append special char for null node
+            return;
         }
-        //logic
-        //return prorder traversal version
-        return root.val + "," + serialize(root.left) + serialize(root.right);
+
+        //process current node - add to serialized string with a delimiter
+        serializedString.append(root.val + ",");
+
+        //explore left and right subtrees
+        serializeHelper(root.left, serializedString);
+        serializeHelper(root.right, serializedString);
+    }
+
+    //time - O(n) - bfs
+    //space - O(n)
+    private void serializeHelperIterative(TreeNode root, StringBuilder serializedString)
+    {
+        Queue<TreeNode> support = new LinkedList<>(); //for bfs
+        support.offer(root);
+
+        while(!support.isEmpty())
+        {
+            int levelSize = support.size();
+            for(int i = 0; i < levelSize; i++)
+            {
+                TreeNode current = support.poll();
+
+                if(current == null)
+                {
+                    serializedString.append("#,"); //append special char for null node
+                    continue;
+                }
+
+                serializedString.append(current.val + ",");
+
+                //children should be processed even if null to maintain structure
+                support.offer(current.left);
+                support.offer(current.right);
+            }
+        }
     }
 
     // Decodes your encoded data to tree.
-    //space - O(n) for the queue
     public TreeNode deserialize(String data) {
-        String encoded = data.substring(0, data.length() - 1); //ignores the last ,
-        System.out.println(encoded);
-        String[] nodes = encoded.split(",");
-        Queue<String> unprocessedNodes = new LinkedList<>(Arrays.asList(nodes)); //using a queue coz of preorder
-        return(deserializeUtil(unprocessedNodes));
+        //populate queue for deserialization
+        serializedTree = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializeHelperIterative();
     }
-    
-    //time - O(n) where n is the number of nodes in tree
-    //sspace - recursive stack - O(h)
-    private TreeNode deserializeUtil(Queue<String> unprocessedNodes) {
-        String front = unprocessedNodes.poll();
-        
+
+    //time - O(n) - preorder traversal
+    //space - O(h)
+    private TreeNode deserializeHelper()
+    {
+        String currentNode = serializedTree.poll(); //get first node from queue
         //base
-        if(front.equals("X"))
+        if(currentNode.equals("#"))
         {
             return null;
         }
-        //logic
-        //build root which is the 1st element in q as it has preorder
-        TreeNode root = new TreeNode(Integer.valueOf(front));
-        //recurse
-        root.left = deserializeUtil(unprocessedNodes);
-        root.right = deserializeUtil(unprocessedNodes);
+
+        //root is the 1st element in queue
+        TreeNode root = new TreeNode(Integer.parseInt(currentNode));
+
+        //build left sub tree
+        root.left = deserializeHelper();
+        
+        //build left sub tree
+        root.right = deserializeHelper();
+
+        return root;
+    }
+
+    //time - O(n) - bfs
+    //space - O(n)
+    private TreeNode deserializeHelperIterative()
+    {
+        String firstNode = serializedTree.poll(); //get the first node
+        if(firstNode.equals("#"))
+        {
+            return null; //empty tree
+        }
+        
+        TreeNode root = new TreeNode(Integer.parseInt(firstNode)); //create root
+        Queue<TreeNode> support = new LinkedList<>(); //for bfs
+        support.offer(root);
+
+        while(!support.isEmpty())
+        {
+            int levelSize = support.size();
+            for(int i = 0; i < levelSize; i++)
+            {
+                TreeNode current = support.poll();
+
+                String leftNode = serializedTree.poll();
+                String rightNode = serializedTree.poll();
+
+                if(!leftNode.equals("#"))
+                {
+                    //valid left node
+                    current.left = new TreeNode(Integer.parseInt(leftNode));
+                    support.offer(current.left); //children of current.left will be added in mext iteration
+                }
+
+                if(!rightNode.equals("#"))
+                {
+                    //valid left node
+                    current.right = new TreeNode(Integer.parseInt(rightNode));
+                    support.offer(current.right); //children of current.right will be added in mext iteration
+                }
+            }
+        }
+
         return root;
     }
 }
-
-// Your Codec object will be instantiated and called as such:
-// Codec codec = new Codec();
-// codec.deserialize(codec.serialize(root));
