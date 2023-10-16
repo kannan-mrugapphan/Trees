c18c336ca450fae4b005359644ab44b7d7b555a8
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-10-15 20:47:50 -0500

    Update LCA.java

--- LCA.java
+++ LCA.java
@@ -55,3 +55,82 @@ class Solution {
         return right;
     }
 }
+
+// 235.
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return findLcaRecursive(root, p, q);
+    }
+
+    //time - O(log n)
+    //space - constant
+    private TreeNode findLca(TreeNode root, TreeNode p, TreeNode q)
+    {
+        //as long as root is not p or q
+        while(root != null)
+        {
+            //check if root is lca
+            //one of p or q lies on left of root and the other is on right of root
+            if((root.val > p.val && root.val < q.val) || (root.val > q.val && root.val < p.val))
+            {
+                return root;
+            }
+
+            //one of the nodes is found
+            //so far from split didn't happen. so the other node will also be decendant of current
+            //so current is lca
+            else if(root == p || root == q)
+            {
+                return root;
+            }
+
+            //both p and q are smaller or larger than root
+            else if(root.val > p.val && root.val > q.val)
+            {
+                //both lies on left of root
+                root = root.left;
+            }
+
+            else if(root.val < p.val && root.val < q.val)
+            {
+                //both lies on right of root
+                root = root.right;
+            }
+        }
+
+        //lca not found
+        return null;
+    }
+
+    //time - O(log n)
+    //space - O(h)
+    private TreeNode findLcaRecursive(TreeNode root, TreeNode p, TreeNode q)
+    {
+        //base
+        if(root == null)
+        {
+            return null; //lca not found as null is reached
+        }
+
+        //check if root is the divergent node
+        if((root.val > p.val && root.val < q.val) || (root.val > q.val && root.val < p.val))
+        {
+            return root; //root is lca as one is on left and the other is on right
+        }
+
+        //check if root is either p or q
+        else if(root == p || root == q)
+        {
+            return root; //the other node is decendant of root, root is lca
+        }
+
+        //both nodes either lie on left or right of root
+        else if(root.val > p.val && root.val > q.val)
+        {
+            return findLcaRecursive(root.left, p, q); //both lie on left
+        }
+
+        //else if(root.val < p.val && root.val < q.val)
+        return findLcaRecursive(root.right, p, q); //both lie on right
+    }
+}
