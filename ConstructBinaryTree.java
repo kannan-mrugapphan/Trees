5aa6456f09de8d4b802a9d816ebf5558c3ef2d59
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2023-10-15 23:13:25 -0500

    Update ConstructBinaryTree.java

--- ConstructBinaryTree.java
+++ ConstructBinaryTree.java
@@ -52,3 +52,62 @@ class Solution {
         return root;
     }
 }
+
+// 889.
+//time - O(n)
+//space - O(n) for map + O(h) for call stack
+class Solution {
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        //edge
+        if(preorder == null || preorder.length == 0 || postorder == null || postorder.length == 0)
+        {
+            return null;
+        }
+
+        //map tracks index of each element in postorder[]
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        for(int i = 0; i < postorder.length; i++)
+        {
+            indexMap.put(postorder[i], i);
+        }
+
+        return buildTree(preorder, 0, preorder.length - 1, postorder, 0, postorder.length - 1, indexMap);
+    }
+
+    //start and end pointers track range of preorder and postorder lists
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] postorder, int postStart, int postEnd, HashMap<Integer, Integer> indexMap)
+    {
+        //base
+        if(preStart > preEnd || postStart > postEnd)
+        {
+            return null;
+        }
+
+        //1st element in preorder list is root
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        //if only 1 element
+        if(preStart == preEnd || postStart == postEnd)
+        {
+            return root;
+        }
+
+        //2nd element in preorder is root of left subtree of current root
+        int leftChild = preorder[preStart + 1];
+        int leftChildIndex = indexMap.get(leftChild);
+
+        //postorder = left, right, root
+        //postorder[postStart, leftChildIndex] is part of left subtree
+        //postorder[leftChildIndex + 1, postEnd - 1] is part of right subtree
+        int leftSubtreeSize = leftChildIndex - postStart + 1; //count in range = end - start + 1
+
+        //preorder[preStart + 1, preStart + leftSubtreeSize] is part of left subtree
+        //preorder[preStart + leftSubtreeSize + 1, preEnd] is part of right subtree
+
+        root.left = buildTree(preorder, preStart + 1, preStart + leftSubtreeSize, postorder, postStart, leftChildIndex, indexMap);
+
+        root.right = buildTree(preorder, preStart + leftSubtreeSize + 1, preEnd, postorder, leftChildIndex + 1, postEnd - 1, indexMap);
+
+        return root;
+    }
+}
